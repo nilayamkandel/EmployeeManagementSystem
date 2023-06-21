d3e85a86b4adb891ee7c1377e88288538349cada
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-21 20:50:43 +0545

    add : user pojo

---
+++ EmployeeSpringProject/src/main/java/com/springproject/employee/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.springproject.employee.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springproject.employee.model.User;
+import com.springproject.employee.service.IUserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private IUserService userService;
+	
+	@GetMapping("/login")
+	public String getLogin() {
+		return "LoginForm";
+	}
+	
+	@PostMapping("/login")
+	public String postLogin(@ModelAttribute User user) {
+		User usr= userService.login(user.getUsername(),user.getPassword());
+		
+		if(usr!= null) {
+			return "Home";
+		
+		}
+		return "LoginForm";
+		
+	}
+}
