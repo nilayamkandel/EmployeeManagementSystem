0ed2cf74751eefc32e761e30ce0de0d916f4368e
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-22 22:16:15 +0545

    department interface

---
+++ EmployeeSpringProject/src/main/java/com/springproject/employee/service/IDepartmentService.java
@@ -0,0 +1,19 @@
+package com.springproject.employee.service;
+
+import java.util.List;
+
+import com.springproject.employee.model.Department;
+
+public interface IDepartmentService {
+	
+	void addDept(Department dept);
+	
+	void deleteDept(int id);
+	
+	void updateDept(Department dept);
+	
+	Department getDeptById(int id);
+	
+	List<Department> getAllDepts();
+	
+}
