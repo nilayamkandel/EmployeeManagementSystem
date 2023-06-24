b186947b71b17f31fc755af14809b364ba07f9fc
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-24 11:32:40 +0545

    method implemented from interface

---
+++ EmployeeSpringProject/src/main/java/com/springproject/employee/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,40 @@
+package com.springproject.employee.service.impl;
+
+import java.util.List;
+
+import com.springproject.employee.model.Employee;
+import com.springproject.employee.service.IEmployeeService;
+
+public class EmployeeServiceImpl implements IEmployeeService {
+
+	@Override
+	public void addEmployee(Employee e) {
+		
+		
+	}
+
+	@Override
+	public void updateEmployee(Employee e) {
+		
+		
+	}
+
+	@Override
+	public void deltEmployee(int id) {
+		
+	}
+
+	@Override
+	public Employee getEmployeeById(int id) {
+		
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAllEmployee() {
+		
+		return null;
+	}
+	
+	
+}
