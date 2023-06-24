c06e45c508c643bb0f0068c12c861a089c253563
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-24 12:21:57 +0545

    method implemented

--- EmployeeSpringProject/src/main/java/com/springproject/employee/service/impl/EmployeeServiceImpl.java
+++ EmployeeSpringProject/src/main/java/com/springproject/employee/service/impl/EmployeeServiceImpl.java
@@ -1,39 +1,48 @@
 package com.springproject.employee.service.impl;
 
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.springproject.employee.model.Employee;
+import com.springproject.employee.repository.EmployeeRepository;
 import com.springproject.employee.service.IEmployeeService;
 
+@Service
 public class EmployeeServiceImpl implements IEmployeeService {
+	
+	@Autowired
+	private EmployeeRepository emplRepo; 
 
 	@Override
 	public void addEmployee(Employee e) {
-		
+		emplRepo.save(e);
 		
 	}
 
 	@Override
 	public void updateEmployee(Employee e) {
-		
+		emplRepo.save(e);
 		
 	}
 
 	@Override
 	public void deltEmployee(int id) {
-		
+		emplRepo.deleteById(id);
 	}
 
 	@Override
 	public Employee getEmployeeById(int id) {
 		
-		return null;
+		return emplRepo.findById(id).get();
 	}
 
 	@Override
 	public List<Employee> getAllEmployee() {
 		
-		return null;
+		return emplRepo.findAll();
 	}
 	
 	
