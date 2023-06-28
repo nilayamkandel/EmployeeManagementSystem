487b17491a88e1cbba478448bbba6605eb099efd
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-28 20:44:38 +0545

    project running code

---
+++ EmployeeSpringProject/src/main/java/com/springproject/employee/EmployeeSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSpringProjectApplication.class, args);
+	}
+
+}
