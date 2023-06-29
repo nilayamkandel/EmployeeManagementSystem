27c99496340bf6ea5d2bbbde8eb6832d0e290025
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-29 20:37:49 +0545

    add: gallery html

--- EmployeeSpringProject/src/main/java/com/springproject/employee/controller/GalleryController.java
+++ EmployeeSpringProject/src/main/java/com/springproject/employee/controller/GalleryController.java
@@ -2,6 +2,8 @@ package com.springproject.employee.controller;
 
 import java.io.File;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class GalleryController {
 
 	@GetMapping("/gallery")
-	public String GetGallery(Model model) {
+	public String GetGallery(Model model, HttpSession session) {
+		
+		if(session.getAttribute("validuser")== null) {
+			return "LoginForm";
+		}
 		
 		String[] imageNames = new File("src/main/resources/static/images").list();
 		model.addAttribute("imgNameList",imageNames);
