1fa3a25886beacfc67f609f76143f4c47945c3fc
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-02 21:29:56 +0545

    log file

--- EmployeeSpringProject/src/main/java/com/springproject/employee/controller/UserController.java
+++ EmployeeSpringProject/src/main/java/com/springproject/employee/controller/UserController.java
@@ -1,9 +1,10 @@
 package com.springproject.employee.controller;
 
-
-
 import javax.servlet.http.HttpSession;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,9 +18,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.springproject.employee.model.User;
 import com.springproject.employee.service.IUserService;
 
+import lombok.extern.java.Log;
+
+@Log
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	//private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired
 	private IUserService userService;
@@ -41,11 +47,13 @@ public class UserController {
 		
 		if(usr != null) {
 			
+			log.info("------------login success------------");
 			session.setAttribute("validuser",usr);
 			session.setMaxInactiveInterval(300);
 			//model.addAttribute("user",usr);	
 			return "Home";
 		}
+		log.info("-------------login failed---------------");
 		model.addAttribute("message","user not found!!");
 		return  "LoginForm";	
 	}
