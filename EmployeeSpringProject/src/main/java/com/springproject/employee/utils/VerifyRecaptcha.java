69341fec9b1ea5e1920add25a95a6597eab5f9fa
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-07 22:05:48 +0545

    static final

---
+++ EmployeeSpringProject/src/main/java/com/springproject/employee/utils/VerifyRecaptcha.java
@@ -0,0 +1,9 @@
+package com.springproject.employee.utils;
+
+public class VerifyRecaptcha {
+	
+	public static final String url = "https://www.google.com/recaptcha/api/siteverify";
+	public static final String secret = "6LcibvwmAAAAAMlv-CP09EpaCOtSMRj8nf2he36V";
+	private final static String USER_AGENT = "Mozilla/5.0";
+
+}
